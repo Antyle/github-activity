38c49114db5f3432eb92ae84aee9fc7088fd7f0e
Antyle <152796256+Antyle@users.noreply.github.com>
2026-06-19 22:07:13 +0200

    feat: added a getusnermae func

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import sys
 import request # is it this ?
 version = 1.0
 
-
-while True:
-    try:
-        username = input("Please enter a github username :")
-    except ValueError:
-        continue
-
-def get_github_info(username):
+def get_username():
+    print(f"Script version {version}")
+    while True:
+        try:
+            username = input("Please enter a github username :")
+        except ValueError:
+            continue
+    print(f"The entered github username is {username}")
     
 
+def get_github_info(username):
+    ...
 if __name__ == '__main__':
-    print(f"Version {version}")
-    print(sys.version) # print python version       
+    get_username()    
