4ed5e5e8aa53c4283894fc6e30eba58a64edcf21
Antyle <152796256+Antyle@users.noreply.github.com>
2026-06-21 20:43:21 +0200

    feat : added quit a lot of thing ehe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import request # is it this ?
+import requests
 version = 1.0
 
 def get_username():
@@ -7,12 +7,30 @@ def get_username():
     while True:
         try:
             username = input("Please enter a github username :")
+            print(f"The entered github username is {username}")
+            print("[DEBUG] : sending username to get_github_info")
+            get_github_info(username)
         except ValueError:
             continue
-    print(f"The entered github username is {username}")
-    
+
 
 def get_github_info(username):
-    ...
+    print("[DEBUG] : asking github api...")
+    api_request(username)
+    print(info)
+
+def api_request(username):
+    api = f"https://api.github.com/users/{username}/events"
+    response = requests.get(api, timeout=10)
+    if response.status_code == 200:
+        print("Request sucessful")
+        global info
+        info = response.json()
+
+
 if __name__ == '__main__':
-    get_username()    
+    if len(sys.argv) > 1:
+        get_github_info(sys.argv[1])
+    else:
+        print("No argument, calling get_username...")
+        get_username()   
