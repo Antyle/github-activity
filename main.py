af5ce22370cd8d77a01a935fb6bc767435524999
Antyle <152796256+Antyle@users.noreply.github.com>
2026-06-26 20:10:55 +0200

    Feat: added error handling and merged get_github_info() to api_request()

--- main.py
+++ main.py
@@ -2,38 +2,37 @@ import sys
 import requests
 version = 1.0
 
+debug = False 
+
 def get_username():
     print(f"Script version {version}")
     while True:
         try:
             username = input("Please enter a github username :")
             print(f"The entered github username is {username}")
-            print("[DEBUG] : sending username to get_github_info")
-            get_github_info(username)
+            if debug == True: print("[DEBUG] : sending username to get_github_info")
+            api_request(username)
         except ValueError:
             continue
 
 
-def get_github_info(username):
-    print("[DEBUG] : asking github api...")
-    api_request(username)
-    activities = info
-    print(f"Activities of {username}")
-    print(f"Pushed to {activity['repo']['name']}")
-
-
 def api_request(username):
     api = f"https://api.github.com/users/{username}/events"
+    if debug == True: print("[DEBUG] : asking github api...")
     response = requests.get(api, timeout=10)
+    if debug == True:  print(response.status_code) #debug only
     if response.status_code == 200:
         print("Request sucessful")
         global info
         info = response.json()
+        print(info)
+    else:
+        print(f"an error occured, error {response.status_code}")
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        get_github_info(sys.argv[1])
+        api_request(sys.argv[1])
     else:
         print("No argument, calling get_username...")
         get_username()   
