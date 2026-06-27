a858c075ee8b11889f83433f28b201cf57b98762
Antyle <152796256+Antyle@users.noreply.github.com>
2026-06-27 17:28:23 +0200

    feat: now printing some infos

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import sys
 import requests
 version = 1.0
-
-debug = False 
+debug = True 
 
 def get_username():
-    print(f"Script version {version}")
     while True:
         try:
             username = input("Please enter a github username :")
@@ -15,22 +13,31 @@ def get_username():
         except ValueError:
             continue
 
-
 def api_request(username):
     api = f"https://api.github.com/users/{username}/events"
     if debug == True: print("[DEBUG] : asking github api...")
     response = requests.get(api, timeout=10)
-    if debug == True:  print(response.status_code) #debug only
+    if debug == True: print(response.status_code) # debug only
     if response.status_code == 200:
         print("Request sucessful")
         global info
         info = response.json()
-        print(info)
+        if debug == True: print(info)
+        display_user_event(info)
     else:
         print(f"an error occured, error {response.status_code}")
 
+def display_user_event(data):
+    if debug == True: print("Data received")
+    activities = data
+    for activity in activities:
+        if activity['type'] == 'WatchEvent':
+            print(f"Followed the repo/organization {activity['repo']['url']}")
+        if activity['type'] == 'PushEvent':
+            print(f'Pushed to {activity['repo']['url']}')
 
 if __name__ == '__main__':
+    print(f"Script version {version}")
     if len(sys.argv) > 1:
         api_request(sys.argv[1])
     else:
